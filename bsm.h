132877a3c8246c1fedced863a1ac79e8bb32f042
Geoff Twardokus <geoff.twardokus@outlook.com>
2022-07-29 16:29:54 -0400

    Add files via upload

---
+++ bsm.h
@@ -0,0 +1,12 @@
+#ifndef CPP_BSM_H
+#define CPP_BSM_H
+
+struct bsm {
+    float latitude;
+    float longitude;
+    float elevation;
+    float speed;
+    float heading;
+};
+
+#endif //CPP_BSM_H
